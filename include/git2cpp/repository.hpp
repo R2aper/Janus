566f6eac22991e28fcfac6272b1121498ca93eee
Mihail <Mihailzharickow@yandex.ru>
2025-02-23 18:59:46 +0700

    Refactored Git::Repository functionality
    Now all Constructors will initialize the repository when creating an
    object
    Added methods:
    - GetIndex and GetTree, for getting Index and Tree from the repository
    - GetCommitHistory and GetBranches, to obtain a list of commits of given
    Branch and all repository's branches
    - BranchExists, checks if given Branch exists
    - Createbranch, to create a branch in the repository
    Rewrited the method for creating a commit

--- include/git2cpp/repository.hpp
+++ include/git2cpp/repository.hpp
@@ -1,16 +1,22 @@
+#pragma once
 #ifndef REPOSITORY_HPP
 #define REPOSITORY_HPP
 
 #include <git2.h>
-#include <git2/repository.h>
 #include <string>
+#include <vector>
+
+#include "git2cpp/commit.hpp"
+#include "git2cpp/index.hpp"
+#include "git2cpp/signature.hpp"
+#include "git2cpp/tree.hpp"
+
 namespace Git {
 
-// .commit
-//. newtree
-// breanch
-// другие команды git связанные с репозиторием
+class Signature;
+class Commit;
 
+// Wrapper for git_repository
 class Repository {
 private:
   git_repository *_repo;
@@ -18,61 +24,144 @@ private:
   bool opened = false;
 
 public:
-  // Открываем существующий репозиторий
-  // Создаем новый
-
-  /**
-   * Constructor. Opens a given Git repository.
+  /* Basic constructor.
    *
-   * @param rep The existing repository that would be open.
-   * @throw Throw a std::runtime_error if the repository cannot be opened.
+   * Initialized and open repository at current directory
+   * @throw Git::Exception if repository cannot be opened or initialized
+   */
+  Repository();
+
+  /* Constructor
+   *
+   * Will open a given repository
+   * @param rep The initialized git repository.
+   * @throw Git::Exception if repository cannot be opened or initialized
    */
   Repository(git_repository *repo);
 
-  // Basic constructor. Create a new repository that is not initialized
-  Repository();
+  /* Constructor.
+   *
+   * Will open a repository
+   *
+   * @param path The path of initialized repository
+   * @throw Git::Exception if repository cannot be opened
+   */
+  Repository(const std::string &path);
 
-  // Destructor. Free git_repository object
+  /* Destructor.
+   *
+   * Free repository if it is not nullptr
+   */
   ~Repository();
 
-  /**
-   * Opens an existing Git repository.
+  /*
+   * Get the Index file for this repository.
    *
-   * @throw Throw a std::runtime_error if the repository cannot be opened.
+   * If a custom index has not been set, the default index for the repository will be returned
+   *
+   * @throw Git::Exception if the getting of index fails
+   * @return index file
    */
-  void Open();
+  Index GetIndex();
 
-  /**
+  /*
+   * Lookup a tree object from the repository.
+   *
+   * @param oid Identity of the tree to locate.
+   * @throw Git::Exception if lookup fails
+   * @return The tree
+   */
+  Tree GetTree(const git_oid &oid);
+
+  /*
+   * @return The path of the repository.
+   */
+  const std::string Path() noexcept;
+
+  /*
    * Checks if the repository is open.
+   *
    * @return True if the repository is open, false otherwise.
    */
-  bool isOpen();
+  bool isOpen() const noexcept;
 
-  /**
+  /*
+   * Checks if the repository is initialized.
+   *
+   * @return True if the repository is initialized, false otherwise.
+   */
+  bool isInit() const noexcept;
+
+  /*
+   * Checks if given branch exists
+   *
+   * @param name The name of the branch
+   * @return True if the branch exists, false otherwise
+   */
+  bool BranchExists(const std::string &name) const noexcept;
+
+  /*
+   * Go through the commit history of a branch
+   *
+   * @param branch_name The name of the branch
+   * @throw Git::Exception if branch does not exist
+   * @return vector of commits
+   */
+  std::vector<Commit> GetCommitHistory(const std::string &branch_name) const;
+
+  /*
+   * List all branches of a repository
+   *
+   * FIX: Should return Git::Exception
+   * @return vector of branch names
+   */
+  std::vector<std::string> GetBranches() const;
+
+  /*
+   * Opens an existing Git repository in given path.
+   *
+   * @throw Throw a Git::Exception if the repository cannot be opened.
+   */
+  void Open(const std::string &path);
+
+  /*
    * Initialize a repository in the given path
+   *
    * @param path The path where the repository will be initialized
-   * @return The initialized repository
-   *@throw Throw a std::runtime_error if the repository cannot be initialized
+   * @throw Throw a Git::Exception if the repository cannot be initialized
    */
   void Init(const std::string &path);
 
-  /**
-   * Checks if the repository is initialized.
-   * @return True if the repository is initialized, false otherwise.
+  /*
+   * Create a new branch
+   *
+   * @paramn name Name for the branch; this name is validated for consistency. It should also not
+   * conflict with an already existing branch name.
+   * @param target Commit to which this branch should point. This object must belong to the this
+   * repository.
+   * @throw Git::Exception if creating branch fails
    */
-  bool isInit();
+  void CreateBranch(const std::string &name, const Commit &target);
 
-  /**
-   * @return The path of the repository.
+  /*
+   * Create a commit in the target branch
+   *
+   * @param message Full message for this commit
+   * @param author Signature with author of commit
+   * @param commiter  Signature with  commiter of the commit
+   * @param index Index with files to commit
+   *
    */
-  const std::string Path();
-
-  // void commit(const std::string &message, Index index, Tree tree, Signature signature);
-
-  void commitAll(const std::string &message);
+  void CreateCommit(const std::string &message, const Signature &author, const Signature &commiter,
+                    const Index &index, const std::string &target_branch = "master");
 
   git_repository *ptr() const;
 
+  Repository(const Repository &) = delete;
+  Repository &operator=(const Repository &) = delete;
+
+  Repository(Repository &&other) noexcept;
+  Repository &operator=(Repository &&other) noexcept;
 };
 
 } // namespace Git
--- src/git2cpp/repository.cpp
+++ src/git2cpp/repository.cpp
@@ -1,22 +1,105 @@
-#include "git2cpp/repository.hpp"
-#include "git2cpp/error.hpp"
 #include <git2.h>
-#include <iostream>
+#include <vector>
+
+#include "git2cpp/commit.hpp"
+#include "git2cpp/error.hpp"
+#include "git2cpp/repository.hpp"
+#include "git2cpp/tree.hpp"
 
 namespace Git {
 
-Repository::Repository(git_repository *repo) : _repo(repo) {}
-Repository::Repository() : _repo(nullptr) {}
-Repository::~Repository() { git_repository_free(_repo); }
-
-void Repository::Open() {
-  if (git_repository_open(&_repo, git_repository_workdir(_repo)) != 0) {
-    throw Git::Exception();
-  }
-  opened = true;
+Repository::Repository() {
+  this->Init("./");
+  this->Open("./");
 }
 
-bool Repository::isOpen() { return opened; }
+Repository::Repository(git_repository *repo) {
+  this->Open(git_repository_workdir(_repo));
+  initialized = true;
+}
+
+Repository::Repository(const std::string &path) {
+  this->Open(path);
+  initialized = true;
+}
+
+Repository::~Repository() {
+  if (_repo)
+    git_repository_free(_repo);
+}
+
+Index Repository::GetIndex() {
+  git_index *_index;
+  if (git_repository_index(&_index, _repo) != 0)
+    throw Exception();
+  return Index(_index);
+}
+
+Tree Repository::GetTree(const git_oid &oid) {
+  git_tree *tree;
+  if (git_tree_lookup(&tree, _repo, &oid) != 0)
+    throw Exception();
+  return Tree(tree);
+}
+
+const std::string Repository::Path() noexcept { return std::string(git_repository_workdir(_repo)); }
+
+bool Repository::isOpen() const noexcept { return opened; }
+bool Repository::isInit() const noexcept { return initialized; }
+
+bool Repository::BranchExists(const std::string &name) const noexcept {
+  git_reference *ref;
+  return git_branch_lookup(&ref, _repo, name.c_str(), GIT_BRANCH_LOCAL) == 0;
+}
+
+std::vector<Commit> Repository::GetCommitHistory(const std::string &branch_name) const {
+  git_revwalk *walker;
+  git_revwalk_new(&walker, _repo);
+  git_revwalk_sorting(walker, GIT_SORT_TOPOLOGICAL | GIT_SORT_REVERSE);
+
+  git_reference *branch_ref;
+  if (git_branch_lookup(&branch_ref, _repo, branch_name.c_str(), GIT_BRANCH_LOCAL) != 0) {
+    git_revwalk_free(walker);
+    throw Exception();
+  }
+  git_revwalk_push_ref(walker, git_reference_name(branch_ref));
+
+  std::vector<Commit> history;
+  git_oid oid;
+  while (git_revwalk_next(&oid, walker) == 0) {
+    git_commit *commit;
+    git_commit_lookup(&commit, _repo, &oid);
+    history.emplace_back(commit);
+  }
+
+  git_reference_free(branch_ref);
+  git_revwalk_free(walker);
+  return history;
+}
+
+std::vector<std::string> Repository::GetBranches() const {
+  std::vector<std::string> branches;
+  git_branch_iterator *iter;
+  git_branch_iterator_new(&iter, _repo, GIT_BRANCH_ALL); // FIX: Can return error code
+
+  git_reference *ref;
+  git_branch_t type;
+  while (git_branch_next(&ref, &type, iter) == 0) {
+    const char *name;
+    git_branch_name(&name, ref); // FIX: Can return error code
+    branches.push_back(name);
+    git_reference_free(ref);
+  }
+
+  git_branch_iterator_free(iter);
+  return branches;
+}
+
+void Repository::Open(const std::string &path) {
+  if (git_repository_open(&_repo, path.c_str()) != 0)
+    throw Git::Exception();
+  opened = true;
+}
 
 void Repository::Init(const std::string &path) {
   if (git_repository_init(&_repo, path.c_str(), false) < 0)
@@ -24,58 +107,53 @@ void Repository::Init(const std::string &path) {
   initialized = true;
 }
 
-bool Repository::isInit() { return initialized; }
-
-const std::string Repository::Path() { return std::string(git_repository_workdir(_repo)); }
-
-git_repository* Repository::ptr() const {return _repo;}
-
-void Repository::commitAll(const std::string &message) {
-  git_index *index;
-  if (git_repository_index(&index, _repo) != 0) {
-    throw Git::Exception();
+void Repository::CreateBranch(const std::string &name, const Commit &target) {
+  git_reference *branch;
+  if (git_branch_create(&branch, _repo, name.c_str(), target.ptr(), 0) != 0) {
+    throw Exception();
   }
+  git_reference_free(branch);
+}
 
-  if (git_index_add_all(index, nullptr, 0, nullptr, nullptr) != 0) {
-    git_index_free(index);
-    throw Git::Exception();
-  }
+void Repository::CreateCommit(const std::string &message, const Signature &author,
+                              const Signature &commiter, const Index &index,
+                              const std::string &target_branch) {
 
-  if (git_index_write(index) != 0) {
-    git_index_free(index);
-    throw Git::Exception();
-  }
+  git_oid tree_id = index.WriteTree();
+  Tree tree = GetTree(tree_id);
 
-  git_oid tree_id;
-  if (git_index_write_tree(&tree_id, index) != 0) {
-    git_index_free(index);
-    throw Git::Exception();
-  }
-
-  git_tree *tree;
-  if (git_tree_lookup(&tree, _repo, &tree_id) != 0) {
-    git_index_free(index);
-    throw Git::Exception();
-  }
-
-  git_signature *signature;
-  if (git_signature_default(&signature, _repo) != 0) {
-    git_tree_free(tree);
-    git_index_free(index);
-    throw Git::Exception();
+  git_reference *branch_ref;
+  Commit parent;
+  if (git_branch_lookup(&branch_ref, _repo, target_branch.c_str(), GIT_BRANCH_LOCAL) ==
+      0) { // FIX: Exception?
+    parent.Lookup(*this, branch_ref);
+    git_reference_free(branch_ref);
   }
 
   git_oid commit_id;
-  if (git_commit_create_v(&commit_id, _repo, "HEAD", signature, signature, nullptr, message.c_str(),
-                          tree, 0) != 0) {
-    git_tree_free(tree);
-    git_index_free(index);
-    git_signature_free(signature);
-    throw Git::Exception();
-  }
+  // const std::vector<Commit> parents; //FIX:
 
-  git_tree_free(tree);
-  git_index_free(index);
-  git_signature_free(signature);
+  const git_commit *parents[] = {parent.ptr()};
+  std::string full_ref = "refs/heads/" + target_branch;
+
+  if (git_commit_create(&commit_id, _repo, full_ref.c_str(), author.ptr(), commiter.ptr(), nullptr,
+                        message.c_str(), tree.ptr(), parent.ptr() ? 1 : 0, parents) != 0) {
+    throw Exception();
+  }
 }
+
+git_repository *Repository::ptr() const { return _repo; }
+
+Repository::Repository(Repository &&other) noexcept : _repo(other._repo) { other._repo = nullptr; }
+
+Repository &Repository::operator=(Repository &&other) noexcept {
+  if (this != &other) {
+    if (_repo)
+      git_repository_free(_repo);
+    _repo = other._repo;
+    other._repo = nullptr;
+  }
+  return *this;
+}
+
 } // namespace Git

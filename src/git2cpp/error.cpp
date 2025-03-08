7716903bb2f73957c805578475ea7a6b6db0c1b2
Mihail <Mihailzharikow@yandex.ru>
2025-03-08 21:49:11 +0700

    Added "Git error" before all errors cause of throwing Git::Exception

--- src/git2cpp/error.cpp
+++ src/git2cpp/error.cpp
@@ -4,6 +4,8 @@
 
 namespace Git {
 
-Exception::Exception() : std::runtime_error(git_error_last() ? git_error_last()->message: "Unknown Git error!") {}
+Exception::Exception()
+    : std::runtime_error(git_error_last() ? ("Git error: " + std::string(git_error_last()->message))
+                                          : "Unknown Git error!") {}
 
 } // namespace Git

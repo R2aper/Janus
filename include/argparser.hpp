68366138f35fd028d6a494e04922b0a438db00b5
Mihail <Mihailzharikow@yandex.ru>
2025-03-11 17:21:51 +0700

    Added ArgParser class for parsing flags and options as arguments in command line

--- include/argparser.hpp
+++ include/argparser.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -19,6 +21,69 @@ public:
   std::vector<std::string> names;
 };
 
-class ArgParser {};
+class ArgParser {
+private:
+  std::map<std::string, std::shared_ptr<ArgInfo>> args_map;
 
+  /*
+   * Get all values after MULTIPLEOPTION and write them to values
+   *
+   * @param arg_info Argument info of MULTIPLEOPTION
+   * @param argc Number of arguments
+   * @param argv Array of arguments
+   */
+  void get_multiple_values(ArgInfo *arg_info, int &index, int argc, char *argv[]);
+
+public:
+  // Constructors
+  ArgParser() = default;
+  ArgParser(std::map<std::string, std::shared_ptr<ArgInfo>> args_map);
+
+  /*
+   * Add argument to args_map
+   *
+   * @param short_name Short name of argument
+   * @param long_name Long name of argument
+   * @param type Type of argument
+   */
+  void AddArg(const std::string &short_name, const std::string &long_name, ArgType type) noexcept;
+
+  /*
+   * Parse command line arguments
+   *
+   * @param argc Number of arguments
+   * @param argv Array of arguments
+   * @throw std::invalid_argument If argument is not found in args_map
+   * @throw std::invalid_argument If argument has invalid type
+   * @throw std::runtime_error If MULTIPLEOPTION or OPTION argument has no values
+   */
+  void Parse(int argc, char *argv[]);
+
+  /*
+   * Check if argument is set
+   *
+   * @param name Argument name
+   * @return true if argument is set
+   */
+  bool isSet(const std::string &name) const noexcept;
+
+  /*
+   * Get value passed to argument(For OPTION)
+   *
+   * @param name Argument name
+   * @return Value passed to argument
+   */
+  std::string getValue(const std::string &name) const noexcept;
+
+  /*
+   * Get values passed to argument
+   *
+   * @param name Argument name
+   * @return Values passed to argument
+   */
+  std::vector<std::string> getValues(const std::string &name) const noexcept;
+
+  // return args_map
+  std::map<std::string, std::shared_ptr<ArgInfo>> getMap() const;
+};
 } // namespace Janus
--- src/argparser.cpp
+++ src/argparser.cpp
@@ -1,2 +1,98 @@
+#include <stdexcept>
 
 #include "argparser.hpp"
+
+namespace Janus {
+
+void ArgParser::get_multiple_values(ArgInfo *arg_info, int &index, int argc, char *argv[]) {
+  index++;
+
+  while (index < argc && !args_map.count(argv[index])) {
+    arg_info->values.push_back(argv[index]);
+    index++;
+  }
+
+  --index;
+}
+
+ArgParser::ArgParser(std::map<std::string, std::shared_ptr<ArgInfo>> args_map)
+    : args_map(args_map) {}
+
+void ArgParser::AddArg(const std::string &short_name, const std::string &long_name,
+                       ArgType type) noexcept {
+
+  auto arg_info = std::make_shared<ArgInfo>();
+  arg_info->type = type;
+
+  if (!short_name.empty()) {
+    arg_info->names.push_back(short_name);
+    args_map[short_name] = arg_info;
+  }
+  if (!long_name.empty()) {
+    arg_info->names.push_back(long_name);
+    args_map[long_name] = arg_info;
+  }
+}
+
+void ArgParser::Parse(int argc, char *argv[]) {
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+    auto it = args_map.find(arg);
+
+    if (it != args_map.end()) {
+      ArgInfo *arg_info = it->second.get();
+
+      switch (arg_info->type) {
+      case ArgType::FLAG:
+        arg_info->is_set = true;
+        break;
+      case ArgType::MULTIPLEOPTION:
+        if (!(i + 1 < argc))
+          throw std::runtime_error("Error: missing value for " + arg);
+
+        get_multiple_values(arg_info, i, argc, argv);
+        arg_info->is_set = true;
+        break;
+      case ArgType::OPTION:
+        if (!(i + 1 < argc))
+          throw std::runtime_error("Error: missing value for " + arg);
+
+        arg_info->values.push_back(argv[++i]);
+        arg_info->is_set = true;
+        break;
+      default:
+        throw std::runtime_error("Unsupported argument type" + arg);
+      }
+
+    } else
+      throw std::runtime_error("Unknown argument " + arg);
+  }
+}
+
+bool ArgParser::isSet(const std::string &name) const noexcept {
+  auto it = args_map.find(name);
+  if (it != args_map.end())
+    return it->second->is_set;
+
+  return false;
+}
+
+std::string ArgParser::getValue(const std::string &name) const noexcept {
+  auto it = args_map.find(name);
+  if (it != args_map.end() && it->second->type == ArgType::OPTION)
+    return it->second->values.front();
+
+  return {};
+}
+
+std::vector<std::string> ArgParser::getValues(const std::string &name) const noexcept {
+  auto it = args_map.find(name);
+  if (it != args_map.end() && it->second->type == ArgType::MULTIPLEOPTION)
+    return it->second->values;
+
+  return {};
+}
+
+std::map<std::string, std::shared_ptr<ArgInfo>> ArgParser::getMap() const { return args_map; }
+
+} // namespace Janus
